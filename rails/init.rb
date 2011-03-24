a62f06bf9d7c0a63a4f63258220036b1ddbc3e41
BenZhang <yantao.zhang@student.adelaide.edu.au>
2011-03-24 23:41:08 +1030

    change

--- rails/init.rb
+++ rails/init.rb
@@ -2,8 +2,8 @@
 require 'tree_view'
 require 'fileutils'
 
-File.copy(File.dirname(__FILE__)+'/ChartShape.png', File.join(Rails.root, 'public', 'images'))
-File.copy(File.dirname(__FILE__)+'/jquery-1.4.2.min.js', File.join(Rails.root, 'public', 'javascripts'))
-File.copy(File.dirname(__FILE__)+'/jquery.svg.min.js', File.join(Rails.root, 'public', 'javascripts'))
-File.copy(File.dirname(__FILE__)+'/jquery.drawinglibrary.js', File.join(Rails.root, 'public', 'javascripts'))
-File.copy(File.dirname(__FILE__)+'/jquery.dimensions.min.js', File.join(Rails.root, 'public', 'javascripts'))
+FileUtils.cp(File.dirname(__FILE__)+'/ChartShape.png', File.join(Rails.root, 'public', 'images'))
+FileUtils.cp(File.dirname(__FILE__)+'/jquery-1.4.2.min.js', File.join(Rails.root, 'public', 'javascripts'))
+FileUtils.cp(File.dirname(__FILE__)+'/jquery.svg.min.js', File.join(Rails.root, 'public', 'javascripts'))
+FileUtils.cp(File.dirname(__FILE__)+'/jquery.drawinglibrary.js', File.join(Rails.root, 'public', 'javascripts'))
+FileUtils.cp(File.dirname(__FILE__)+'/jquery.dimensions.min.js', File.join(Rails.root, 'public', 'javascripts'))
