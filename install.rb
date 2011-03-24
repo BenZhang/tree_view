006d6372f3cc08905b37878aa514b2736e9e0562
BenZhang <yantao.zhang@student.adelaide.edu.au>
2011-03-24 23:32:28 +1030

    update

--- install.rb
+++ install.rb
@@ -1,1 +1,7 @@
 # Install hook code here
+
+File.copy(File.dirname(__FILE__)+'/rails/ChartShape.png', File.join(Rails.root, 'public', 'images'))
+File.copy(File.dirname(__FILE__)+'/rails/jquery-1.4.2.min.js', File.join(Rails.root, 'public', 'javascripts'))
+File.copy(File.dirname(__FILE__)+'/rails/jquery.svg.min.js', File.join(Rails.root, 'public', 'javascripts'))
+File.copy(File.dirname(__FILE__)+'/rails/jquery.drawinglibrary.js', File.join(Rails.root, 'public', 'javascripts'))
+File.copy(File.dirname(__FILE__)+'/rails/jquery.dimensions.min.js', File.join(Rails.root, 'public', 'javascripts'))
