5a2d88048899ba62960ae8a268bbc24192ddf694
BenZhang <yantao.zhang@student.adelaide.edu.au>
2011-03-24 23:05:09 +1030

    add jQuery

--- rails/init.rb
+++ rails/init.rb
@@ -3,3 +3,7 @@ require 'tree_view'
 require 'fileutils'
 
 File.copy(File.dirname(__FILE__)+'/ChartShape.png', File.join(Rails.root, 'public', 'images'))
+File.copy(File.dirname(__FILE__)+'/jquery-1.4.2.min.js', File.join(Rails.root, 'public', 'javascripts'))
+File.copy(File.dirname(__FILE__)+'/jquery.svg.min.js', File.join(Rails.root, 'public', 'javascripts'))
+File.copy(File.dirname(__FILE__)+'/jquery.drawinglibrary.js', File.join(Rails.root, 'public', 'javascripts'))
+File.copy(File.dirname(__FILE__)+'/jquery.dimensions.min.js', File.join(Rails.root, 'public', 'javascripts'))
