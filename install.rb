3cca63fb599e3c559f3625621615306428399ed2
BenZhang <yantao.zhang@student.adelaide.edu.au>
2011-03-25 23:10:31 +1030

    fix bug

--- install.rb
+++ install.rb
@@ -1,8 +1,1 @@
 # Install hook code here
-require 'fileutils'
-
-FileUtils.cp(File.dirname(__FILE__)+'/asset/ChartShape.png', File.join(Rails.root, 'public', 'images'))
-FileUtils.cp(File.dirname(__FILE__)+'/asset/jquery-1.4.2.min.js', File.join(Rails.root, 'public', 'javascripts'))
-FileUtils.cp(File.dirname(__FILE__)+'/asset/jquery.svg.min.js', File.join(Rails.root, 'public', 'javascripts'))
-FileUtils.cp(File.dirname(__FILE__)+'/asset/jquery.drawinglibrary.js', File.join(Rails.root, 'public', 'javascripts'))
-FileUtils.cp(File.dirname(__FILE__)+'/asset/jquery.dimensions.min.js', File.join(Rails.root, 'public', 'javascripts'))
