699072e116900b1d8110f467d4ad4728a5360d35
BenZhang <yantao.zhang@student.adelaide.edu.au>
2011-03-25 23:02:13 +1030

    change installation

--- install.rb
+++ install.rb
@@ -1,7 +1,8 @@
 # Install hook code here
+require 'fileutils'
 
-File.copy(File.dirname(__FILE__)+'/rails/ChartShape.png', File.join(Rails.root, 'public', 'images'))
-File.copy(File.dirname(__FILE__)+'/rails/jquery-1.4.2.min.js', File.join(Rails.root, 'public', 'javascripts'))
-File.copy(File.dirname(__FILE__)+'/rails/jquery.svg.min.js', File.join(Rails.root, 'public', 'javascripts'))
-File.copy(File.dirname(__FILE__)+'/rails/jquery.drawinglibrary.js', File.join(Rails.root, 'public', 'javascripts'))
-File.copy(File.dirname(__FILE__)+'/rails/jquery.dimensions.min.js', File.join(Rails.root, 'public', 'javascripts'))
+FileUtils.cp(File.dirname(__FILE__)+'/asset/ChartShape.png', File.join(Rails.root, 'public', 'images'))
+FileUtils.cp(File.dirname(__FILE__)+'/asset/jquery-1.4.2.min.js', File.join(Rails.root, 'public', 'javascripts'))
+FileUtils.cp(File.dirname(__FILE__)+'/asset/jquery.svg.min.js', File.join(Rails.root, 'public', 'javascripts'))
+FileUtils.cp(File.dirname(__FILE__)+'/asset/jquery.drawinglibrary.js', File.join(Rails.root, 'public', 'javascripts'))
+FileUtils.cp(File.dirname(__FILE__)+'/asset/jquery.dimensions.min.js', File.join(Rails.root, 'public', 'javascripts'))
