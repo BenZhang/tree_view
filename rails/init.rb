943ad8b8f569d1990bc4bf42c18bca7386a2e688
BenZhang <yantao.zhang@student.adelaide.edu.au>
2011-03-25 23:02:33 +1030

    change

--- rails/init.rb
+++ rails/init.rb
@@ -1,9 +1,2 @@
 # Include hook code here
 require 'tree_view'
-require 'fileutils'
-
-FileUtils.cp(File.dirname(__FILE__)+'/ChartShape.png', File.join(Rails.root, 'public', 'images'))
-FileUtils.cp(File.dirname(__FILE__)+'/jquery-1.4.2.min.js', File.join(Rails.root, 'public', 'javascripts'))
-FileUtils.cp(File.dirname(__FILE__)+'/jquery.svg.min.js', File.join(Rails.root, 'public', 'javascripts'))
-FileUtils.cp(File.dirname(__FILE__)+'/jquery.drawinglibrary.js', File.join(Rails.root, 'public', 'javascripts'))
-FileUtils.cp(File.dirname(__FILE__)+'/jquery.dimensions.min.js', File.join(Rails.root, 'public', 'javascripts'))
